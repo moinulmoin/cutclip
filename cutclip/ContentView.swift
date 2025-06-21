411406d635fa3b68edf8e5213c72b8c1b7062ea8
Moinul Moin <moinulmoin101@gmail.com>
2025-06-21 23:13:53 +0600

    Initial Commit

---
+++ cutclip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cutclip
+//
+//  Created by Moinul Moin on 6/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cutclip/cutclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  cutclipApp.swift
+//  cutclip
+//
+//  Created by Moinul Moin on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct cutclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
